19b2b24f34857e8d50553daa60980223fa5c5aca
PePe <you@example.com>
2018-07-20 21:55:53 -0500

    primer maquetadoweb

---
+++ js/router-config.js
@@ -0,0 +1,28 @@
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templetes/main.htm"
+    })
+    .when("/proyectos", {
+        templateUrl : "templetes/proyectos.htm"
+    })
+    .when("/contacto", {
+        templateUrl : "templetes/contacto.htm"
+    })
+    .when("/quienessomos", {
+        templateUrl : "templetes/quienessomos.htm"
+    })
+    .when("/comodonar", {
+        templateUrl : "templetes/comodonar.htm"
+    })
+    .when("/sitiosdeinteres", {
+        templateUrl : "templetes/sitiosdeinteres.htm"
+    })
+    .when("/creaunacolecta", {
+        templateUrl : "templetes/creaunacolecta.htm"
+
+    .when("/login", {
+            templateUrl : "./templetes/login.htm"
+        })
+    });
+});
